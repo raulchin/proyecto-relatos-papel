e4696344fe27be6b92bf06c13e4310b7a593e3cd
ba0100062m <rchin@baustro.fin.ec>
2026-01-19 18:02:11 -0500

    Crear eureka

---
+++ backend/registry-server/src/test/java/com/relatospapel/registry_server/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatospapel.registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
