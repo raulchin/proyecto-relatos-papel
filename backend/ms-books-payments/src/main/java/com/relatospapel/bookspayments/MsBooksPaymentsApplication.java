71fb3ec4567a652d7177c6bf158c426ef7b55688
ba0100062m <rchin@baustro.fin.ec>
2026-01-30 16:55:56 -0500

    Crear nuevo ms y configuracion de las rutas

---
+++ backend/ms-books-payments/src/main/java/com/relatospapel/bookspayments/MsBooksPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.relatospapel.bookspayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBooksPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBooksPaymentsApplication.class, args);
+	}
+
+}
--- backend/ms-gateway/src/main/java/com/relatospapel/msgateway/beans/GatewayBeans.java
+++ backend/ms-gateway/src/main/java/com/relatospapel/msgateway/beans/GatewayBeans.java
@@ -4,21 +4,30 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GatewayBeans {
 
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    @Profile( value = "eureka-off" )
+    public RouteLocator routeLocatorEurekaOff(RouteLocatorBuilder builder) {
         return builder
                 .routes()
-                .route(route -> route
-                        .path("")
-                        .uri("")
-                )
-                .route(route -> route
-                        .path("")
-                        .uri("")
+                .route("books", route -> route
+                        .path("/v1/books", "/v1/books/**")
+                        .uri("http://127.0.0.1:8999")
+                ).build();
+    }
+
+    @Bean
+    @Profile( value = "eureka-on" )
+    public RouteLocator routeLocatorEurekaOn(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route("books", route -> route
+                        .path("/v1/books", "/v1/books/**")
+                        .uri("lb://ms-books-catalogue")
                 ).build();
     }
 }
